a2fab779f727d63dd7375fa79143f9f754f3f0b1
mj10021 <jamesthespeedy@gmail.com>
2023-08-31 13:38:20 -0400

    idk

--- src/parse/feature_finder/mod.rs
+++ src/parse/feature_finder/mod.rs
@@ -7,16 +7,16 @@ pub enum Feature {
     Retraction,
     DeRetraction,
 }
-#[derive(Debug)]
-struct Shape {
+#[derive(Clone, Debug)]
+pub struct Shape {
     start_vtx: i32,
     end_vtx: i32,
     z: f32,
     len: f32,
 }
 impl Shape {
-    fn build_planar(gcode: &Parsed) -> VecDeque<Shape> {
-        let mut out = VecDeque::new();
+    pub fn build_planar(gcode: &Parsed) -> Vec<Shape> {
+        let mut out = Vec::new();
         let mut start_vtx = -1;
         let mut in_shape = false;
         let mut dist = 0.0;
@@ -43,7 +43,7 @@ impl Shape {
                                 len: dist,
                                 z: v.to.z,
                             };
-                            out.push_back(s);
+                            out.push(s);
                             dist = 0.0;
                             start_vtx = -1;
                         }
@@ -55,10 +55,10 @@ impl Shape {
         out
     }
     fn get_center(&self) -> (f32, f32, f32) {
-        
+        todo!();
     }
 }
-struct Layer {
+pub struct Layer {
     num: i32,
     start_id: i32,
     end_id: i32,
@@ -77,29 +77,36 @@ impl std::fmt::Debug for Layer {
 impl Layer {
     // FIXME: layer and shape should be maps of vertex to shape and
     // either vertex to layer or shape to layer not sure
-    fn build_planar(gcode: &Parsed) -> Vec<Layer> {
+    pub fn build_planar(gcode: &Parsed) -> HashMap<i32, Layer> {
         let shapes = Shape::build_planar(gcode);
-        let mut out = Vec::new();
+        let mut out = HashMap::new();
         let mut i = 0;
         let mut curr_z = 0.0;
         let mut temp_shapes: Vec<Shape> = Vec::new();
         for shape in shapes {
             if shape.z != curr_z {
                 if temp_shapes.len() > 0 {
-                    out.push(Layer {
-                        num: i,
-                        start_id: temp_shapes[0].start_vtx,
-                        end_id: temp_shapes[temp_shapes.len() - 1].end_vtx,
-                        shapes: temp_shapes,
-                        z: curr_z,
-                    });
+                    let start = temp_shapes[0].start_vtx;
+                    let end = temp_shapes[temp_shapes.len() - 1].end_vtx;
+                    for Shape { z: _, start_vtx: s, end_vtx: f, len: _ } in temp_shapes.iter() {
+                        for j in *s..=*f {
+                            out.insert(j, Layer {
+                                num: i,
+                                start_id: start,
+                                end_id: end,
+                                shapes: temp_shapes.clone(),
+                                z: curr_z,
+                            });
+                        }
+
+                    }
                 }
                 i += 1;
                 curr_z = shape.z;
                 temp_shapes = Vec::new();
-                temp_shapes.push(shape);
+                temp_shapes.push(shape.clone());
             } else {
-                temp_shapes.push(shape);
+                temp_shapes.push(shape.clone());
             }
         }
         out
@@ -114,8 +121,8 @@ fn shape_finder_test() {
 #[test]
 fn layer_finder_test() {
     let gcode = Parsed::build("test.gcode").expect("failed to parse");
-    let l = Layer::build_planar(&gcode);
-    let mut l = VecDeque::from(l);
+    let l = gcode.layers;
+    let mut l = l.values().collect::<VecDeque<&Layer>>();
     l.pop_front();
     l.pop_front();
     let mut z = l.pop_front().unwrap().z;
